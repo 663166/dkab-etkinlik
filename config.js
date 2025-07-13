61474f13188f14c1a261dcb8071cbfa42cd848d3
663166 <94318984+663166@users.noreply.github.com>
2025-07-13 20:25:26 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Supabase bağlantı ayarları
+const SUPABASE_URL = "https://toxhhtbedgkhbpogiwrp.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRveGhodGJlZGdraGJwb2dpd3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwNzY2MTIsImV4cCI6MjA2NzY1MjYxMn0.TXhql-C0rnAdn4QiqmS7_dOAWPwGbreVh2O5-DV0o6Q";
+
+// Supabase istemcisi oluşturmak için bu değişkenleri kullanabilirsin
+// Örnek:
+// const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY); 
